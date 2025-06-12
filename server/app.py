82220155e6b39005d38d354e2537f2bfec914a66
Jefferson <jeff.wafula1@student.moringaschool.com>
2025-06-12 07:54:48 +0300

    after completion

--- server/app.py
+++ server/app.py
@@ -1,8 +1,46 @@
-#!/usr/bin/env python3
-
 from flask import Flask
+from markupsafe import escape
+
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return param  
+
+
+@app.route('/count/<int:param>')
+@app.route('/count/<int:param>')
+def count(param):
+    numbers = [str(i) for i in range(param)]
+    return '\n'.join(numbers) + '\n'
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 == 0:
+            return 'Error: Division by zero is not allowed.', 400
+        result = num1 / num2
+    elif operation == '%':
+        if num2 == 0:
+            return 'Error: Modulo by zero is not allowed.', 400
+        result = num1 % num2
+    else:
+        return 'Invalid operation', 400
+
+    return str(result)
+
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
